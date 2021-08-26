4b63ab788c4a0a86d7fda3dbe5d16d44ea090cb4
Finley Sherwood <me@itsf.in>
2021-08-26 16:24:41 +1000

    Complete exercise 4.1

---
+++ week_05/Exercise4_1.java
@@ -0,0 +1,17 @@
+public class Exercise4_1 {
+    public static void main(String[] args) {
+        Exercise4_1.printCapitalized("Now is the time to act!");
+    }
+
+    public static void printCapitalized(String sentence) {
+        char[] letters = sentence.toCharArray();
+
+        for (int i = 0; i < letters.length; i++) {
+            if (Character.isLetter(letters[i]) && i > 0 ? !Character.isLetter(letters[i-1]) : false) {
+                letters[i] = Character.toUpperCase(letters[i]);
+            }
+        }
+
+        System.out.println(new String(letters));
+    }
+}

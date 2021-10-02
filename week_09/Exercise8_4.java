e31c85e8312f2ecc589c8ce885a2ea7e13334336
Finley Sherwood <me@itsf.in>
2021-10-02 15:04:34 +1000

    Complete exercise 8.4

---
+++ week_09/Exercise8_4.java
@@ -0,0 +1,39 @@
+import textio.TextIO;
+import expr.Expr;
+
+public class Exercise8_4 {
+    public static void main(String[] args) {
+        System.out.println("Calculate expressions using \"x\" as the variable.");
+
+        while (true) {
+            System.out.print("Expression: ");
+            String expression = TextIO.getln();
+
+            if (expression.length() == 0)
+                break;
+
+            Expr expr = new Expr(expression);
+
+            while (true) {
+                System.out.print("Value: ");
+                String raw = TextIO.getln().trim();
+                double value;
+
+                if (raw.length() == 0)
+                    break;
+                
+                try {
+                    value = Double.parseDouble(raw);
+                } catch (NumberFormatException e) {
+                    System.out.println("The provided value was not valid.");
+                    continue;
+                }
+
+                double result = expr.value(value);
+                System.out.printf("f(%s) = %s\n", raw, result);
+            }
+        }
+
+        System.out.println("That's enough maths for now.");
+    }
+}
